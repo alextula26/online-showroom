38eb58f34e8e3c4e3e85a48367b6e9b58b2b74b0
alextula26 <alex_marc86@mail.ru>
2020-12-06 23:02:58 +0300

    refactoring VehicleCarousel component

--- src/components/commons/VehicleCarousel.jsx
+++ src/components/commons/VehicleCarousel.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import cn from 'classnames';
+import { Carousel } from 'react-bootstrap';
 import { uniqueId } from '../../utils';
 
 class VehicleCarousel extends React.Component {
@@ -15,94 +15,33 @@ class VehicleCarousel extends React.Component {
     };
   }
 
-  renderCarouselItem(vehicleFullName, vehicleId) {
-    const { carouselCurrentIdx, images } = this.state;
-
-    return (
-      images.map(({ full, key }, index) => {
-        const classesCarousel = cn({
-          'carousel-item': true,
-          item: true,
-          active: carouselCurrentIdx === index,
-        });
-
-        return (
-          <div key={key} className={classesCarousel}>
-            <a
-              href={full}
-              data-fancybox={`gallery${vehicleId}`}
-            >
-              <img
-                src={full}
-                alt={`${vehicleFullName} Тестовый дилер Тула`}
-                rel="nofollow"
-              />
-            </a>
-          </div>
-        );
-      })
-    );
-  }
-
-  renderCarouselIndicatorsItem(vehicleFullName, vehicleId) {
-    const { carouselCurrentIdx, images } = this.state;
-
-    return (
-      images.map(({ full, key }, index) => {
-        const classesCarouselIndicators = cn({
-          visible: true,
-          active: carouselCurrentIdx === index,
-        });
-
-        return (
-          <li
-            key={key}
-            className={classesCarouselIndicators}
-            data-target={`#js-carousel-${vehicleId}`}
-            data-slide-to={index}
-          >
-            <a href={full}>
-              <img
-                src={full}
-                alt={`${vehicleFullName} Тестовый дилер Тула`}
-                rel="nofollow"
-              />
-            </a>
-          </li>
-        );
-      })
-    );
-  }
+  handleSelect = (selectedIndex) => {
+    this.setState({ carouselCurrentIdx: selectedIndex });
+  };
 
   render() {
-    const { vehicleFullName, vehicleId } = this.props;
+    const { vehicleFullName } = this.props;
+    const { carouselCurrentIdx, images } = this.state;
 
     return (
       <div className="autocrm10-carousel">
-        <div
-          id={`js-carousel-${vehicleId}`}
-          className="carousel-fade slide carousel"
-          data-interval="false"
+        <Carousel
+          className="carousel-fade"
+          interval={999999}
+          activeIndex={carouselCurrentIdx}
+          onSelect={this.handleSelect}
+          nextIcon={<span className="svg--icon svg--arrow-alt2" data-grunticon-embed aria-hidden="true" />}
+          prevIcon={<span className="svg--icon svg--arrow-alt2" data-grunticon-embed aria-hidden="true" />}
         >
-          <div className="carousel-inner">
-            {this.renderCarouselItem(vehicleFullName, vehicleId)}
-          </div>
-
-          <ol className="carousel-indicators">
-            {this.renderCarouselIndicatorsItem(vehicleFullName, vehicleId)}
-          </ol>
-
-          <a className="left carousel-control" href={`#js-carousel-${vehicleId}`} data-slide="prev">
-            <span className="svg--icon svg--arrow-alt2" data-grunticon-embed aria-hidden="true" />
-          </a>
-          <a className="right carousel-control" href={`#js-carousel-${vehicleId}`} data-slide="next">
-            <span className="svg--icon svg--arrow-alt2" data-grunticon-embed aria-hidden="true" />
-          </a>
-          <div className="carousel-counter">
-            <span className="current-slide">1</span>/<span className="all-slides">4</span>
-          </div>
-
-        </div>
+          {images.map(({ full, key }) => (
+            <Carousel.Item key={key}>
+              <img
+                src={full}
+                alt={`${vehicleFullName} Тестовый дилер Тула`}
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
       </div>
     );
   }
