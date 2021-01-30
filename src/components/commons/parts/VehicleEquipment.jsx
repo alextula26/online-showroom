36f4e75ae31d8883e1d2df9b54d26b0f35026e85
alextula26 <alex_marc86@mail.ru>
2021-01-30 13:40:52 +0300

    update VehicleEquipment component

--- src/components/commons/parts/VehicleEquipment.jsx
+++ src/components/commons/parts/VehicleEquipment.jsx
@@ -1,80 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Collapse } from 'react-bootstrap';
 import cn from 'classnames';
 import { uniqueId, includes, getHtml } from '../../../utils';
 
-class VehicleEquipment extends React.Component {
-  constructor(props) {
-    super(props);
+const VehicleEquipment = ({ options }) => {
+  const [indexes, setIndex] = useState([]);
 
-    this.state = {
-      indicesCollapse: [],
-    };
-  }
-
-  setIndexCollapse = (index) => {
-    const { indicesCollapse } = this.state;
-
-    if (!includes(indicesCollapse, index)) {
-      this.setState({ indicesCollapse: [...indicesCollapse, index] });
+  const handleSetIndex = (selectedIndex) => {
+    if (!includes(indexes, selectedIndex)) {
+      setIndex([...indexes, selectedIndex]);
     } else {
-      this.setState({ indicesCollapse: indicesCollapse.filter((item) => item !== index) });
+      setIndex(indexes.filter((index) => index !== selectedIndex));
     }
-  }
+  };
 
-  render() {
-    const { indicesCollapse } = this.state;
-    const { options } = this.props;
+  return (
+    <div className="panel-group">
+      {options.map((option, index) => {
+        const isIncludes = includes(indexes, index);
+        const classes = cn({
+          'collapse-toggle': true,
+          collapsed: !isIncludes,
+        });
 
-    return (
-      <div className="panel-group">
-        {options.map((option, index) => {
-          const isIncludes = includes(indicesCollapse, index);
-          const classes = cn({
-            'collapse-toggle': true,
-            collapsed: !isIncludes,
-          });
-
-          return (
-            <div
-              key={`group_${uniqueId()}`}
-              className="panel panel-default"
-            >
-              <div className="panel-heading">
-                <h4 className="panel-title">
-                  <a
-                    className={classes}
-                    href={`#equipment-group-collapse-${index}`}
-                    aria-expanded={isIncludes}
-                    aria-controls={`equipment-group-${index}`}
-                    onClick={() => this.setIndexCollapse(index)}
-                  >
-                    {option.group}
-                  </a>
-                </h4>
-              </div>
-
-              <Collapse in={isIncludes}>
-                <div id={`equipment-group-${index}`}>
-                  <ul className="list-group">
-                    {option.options.map((html) => (
-                      <li
-                        key={`option_${uniqueId()}`}
-                        className="list-group-item"
-                      >
-                        {getHtml(html)}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </Collapse>
-
+        return (
+          <div
+            key={`group_${uniqueId()}`}
+            className="panel panel-default"
+          >
+            <div className="panel-heading">
+              <h4 className="panel-title">
+                <a
+                  className={classes}
+                  href={`#equipment-group-collapse-${index}`}
+                  aria-expanded={isIncludes}
+                  aria-controls={`equipment-group-${index}`}
+                  onClick={() => handleSetIndex(index)}
+                >
+                  {option.group}
+                </a>
+              </h4>
             </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+
+            <Collapse in={isIncludes}>
+              <div id={`equipment-group-${index}`}>
+                <ul className="list-group">
+                  {option.options.map((html) => (
+                    <li
+                      key={`option_${uniqueId()}`}
+                      className="list-group-item"
+                    >
+                      {getHtml(html)}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </Collapse>
+
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default VehicleEquipment;
