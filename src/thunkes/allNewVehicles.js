fb362f1245e2269775bc87d69453394bd53b2f38
alextula26 <alex_marc86@mail.ru>
2021-02-16 22:22:40 +0300

    get all vehicles

--- src/thunkes/allNewVehicles.js
+++ src/thunkes/allNewVehicles.js
@@ -1,15 +1,50 @@
-/* import CONST from '../utils/const';
 import API from '../api';
+import getVehicles from './vehicles';
+/* import CONST from '../utils/const';
 import * as actions from '../actions';
 import {
   getLisFilterItems, getColorsListFilter, getMinPrice, getMaxPrice,
 } from '../utils';
-import getVehicles from './vehicles'; */
+ */
 
 export default (brands, typeVehicles, query) => async () => {
   console.log('brands', brands);
   console.log('typeVehicles', typeVehicles);
   console.log('query', query);
+
+  const modelsByBrands = brands
+    .filter((brand) => brand.vehicles !== 0)
+    .map(async (brand) => {
+      const models = await API.getModels(brand.id);
+      const modelsWithVehiclesIds = models.items
+        .filter((model) => model.min_price > 0)
+        .map((model) => model.id);
+      return modelsWithVehiclesIds;
+    });
+
+  const promisesModelsByBrandsSettled = await Promise.allSettled(modelsByBrands);
+
+  const modelIds = await promisesModelsByBrandsSettled
+    .filter((item) => item.status === 'fulfilled')
+    .reduce((acc, { value }) => [...acc, ...value], []);
+
+  console.log('modelIds', modelIds);
+
+  const vehiclesByModelIds = modelIds.map(async (id) => {
+    const vehicles = await getVehicles(id, typeVehicles, query);
+    return vehicles.items;
+  });
+
+  const promisesVehiclesByModelIdsSettled = await Promise.allSettled(vehiclesByModelIds);
+
+  console.log('promisesVehiclesByModelIdsSettled', promisesVehiclesByModelIdsSettled);
+
+  const vehicles = await promisesVehiclesByModelIdsSettled
+    .filter((item) => item.status === 'fulfilled')
+    .reduce((acc, { value }) => [...acc, ...value], []);
+
+  console.log('vehicles', vehicles);
+
   /* try {
     const vehicles = await getVehicles(modelId, typeVehicles, query);
     const generalListColorsByModel = await API.getModelColor(modelId);
